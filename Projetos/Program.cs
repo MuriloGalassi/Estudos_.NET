2c37a8ecfed0988a0cdc6b14aacfe085fe0b0be1
MuriloGalassi <61015480+MuriloGalassi@users.noreply.github.com>
2024-09-25 13:39:56 -0300

    Add files via upload

---
+++ Projetos/Program.cs
@@ -0,0 +1,54 @@
+﻿using Projetos.Models;
+
+
+
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+//c = c + 5;  // 30 + 5
+
+c += 5;
+
+Console.WriteLine(c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+// string apresentacao = "Ola, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da varievel quantidade:" + quantidade);
+// Console.WriteLine("Valor da varievel altura:" + altura.ToString("0.00"));
+// Console.WriteLine("Valor da varievel preco:" + preco);
+// Console.WriteLine("Valor da varievel condicao:" + condicao);
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "João";
+//pessoa1.Idade = 22;
+//pessoa1.Apresentar();
