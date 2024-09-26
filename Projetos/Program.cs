d9c310993d6e6ef4fdff5088ccf07e3fea2c7fc7
Murilo Galassi <61015480+MuriloGalassi@users.noreply.github.com>
2024-09-26 16:10:14 -0300

    Update Program.cs

--- Projetos/Program.cs
+++ Projetos/Program.cs
@@ -1,17 +1,103 @@
-﻿using Projetos.Models;
+using Projetos.Models;
+
+
+string a = "15-";
+
+//int b = 0;
+
+int.TryParse(a, out int b);
+
+
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso!");
 
 
 
-int a = 10;
-int b = 20;
 
-int c = a + b;
 
-//c = c + 5;  // 30 + 5
 
-c += 5;
 
-Console.WriteLine(c);
+
+
+
+
+
+
+
+
+
+// double a = 4 / (2 + 2);
+
+// Console.WriteLine(a);
+
+
+
+// int a = 5;
+// double b = a;   
+
+// int a = 5;
+// long b = a;
+
+// Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+// Cast - Casting
+//int a = Convert.ToInt32(null);
+//int a = int.Parse(null);
+
+//double.Parse();
+//decimal.Parse();
+
+//Convert.ToInt32();
+
+//Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+// c = c + 5;  // 30 + 5
+
+// c += 5;
+
+//  c = c - 5
+// c *= 5;
+
+// Console.WriteLine(c);
 
 
 
